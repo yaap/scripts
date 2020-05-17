a1494de1470965155096635faf8a4517ba5c029c
Ido Ben-Hur <idoybh2@gmail.com>
2020-05-17 05:14:15 +0300

    scripts: updater: Rewrite
    * Be generic for all devices, get values from the environment
    * Offer uploading build
    * Use generated files in $OUT rather than generating new ones
    * Show diff and ask before pushing

--- updater/updater.sh
+++ updater/updater.sh
@@ -1,45 +1,67 @@
-#
-#Updater script by RunningMango, ver1.0.
-#
-#Description:
-#This script made for parsing timestamp, generating DerpFest zip name
-# (assuming it was built today) and updating Updater-Stuff git repo
-#
-#Installing:
-# $ mv updater.sh ~/updater.sh
-# $ chmod +x updater.sh
-# After this, edit lines 1-5 to match your device and paths and run.
-#
-#Note: check case of (c/C)hangelog.txt in line 41! It should match
-# your git changelog.txt
-#
+#!/bin/bash
 
+# Making sure $OUT is populated
+if [[ $OUT == '' ]]; then
+  echo "Please lunch the target device."
+  echo "Also make sure a build exists in out folder"
+  exit 1
+fi
 
-gitPath='Updater-Stuff'		#Path to git repo folder (~/Updater-Stuff/<devices>)
-device='wayne'
-android='10'
-buildtype="Official"
-workdir='derpfest'		#Path to sources (~/derpfest/<sources>)
+REPO="Updater-Stuff" # Name of the update repo
+REMOTE="https://github.com/DerpLab" # URL to the remote git
+BRANCH="master" # Default branch name
+DEVICE=$(basename $OUT) # Device name
+ZIP_PATH=$(find $OUT -maxdepth 1 -type f -name "Derp*.zip" | sed -n -e "1{p;q}")
+ZIP=$(basename $ZIP_PATH)
+DATE=$(echo $ZIP | sed -n -e "s/^.*${DEVICE}-//p")
+DATE="${DATE:0:4}-${DATE:4:2}-${DATE:6:2}"
+# Upload build to SourceForge
+echo -n "Upload ${ZIP} to SourceForge ? y/[n] > "
+read ans
+if [[ $ans == 'y' ]]; then
+  echo -n "Enter SourceForge username: "
+  read userName
+  echo "Uploading build"
+  scp $ZIP_PATH "${userName}@frs.sourceforge.net":"/home/frs/p/derpfest/${DEVICE}"
+  echo "Uploading md5sum"
+  scp "${ZIP_PATH}.md5sum" "${userName}@frs.sourceforge.net":"/home/frs/p/derpfest/${DEVICE}"
+fi
 
-cd $gitPath/$device
-git fetch --all			#Get last version of git repo
-git pull
+# Clonning / Fetchig Updater-Stuff repo if needed be
+if [[ -d $REPO ]]; then
+  cd $REPO
+  git fetch --all
+  git checkout origin/$BRANCH
+else
+  git clone $REMOTE/$REPO
+  cd $REPO
+fi
 
-date=$(date -d "$D" '+%Y')$(date -d "$D" '+%m')$(date -d "$D" '+%d')
-timestamp=`sed -n 8p /home/$USER/$workdir/out/target/product/$device/ota_metadata | cut -d'=' -f2`
-package='DerpFest-'$android'-'$buildtype'-'$device'-'$date'.zip'
-rm $device.json
-echo '{
-  "response": [
-    {
-      "datetime": '$timestamp',
-      "filename": "'$package'"
-    }
-  ]
-}' >> $device.json
-rm changelog.txt
-cp /home/$USER/$workdir/out/target/product/$device/Changelog.txt changelog.txt	#Check case here!
+# Making sure device folder exists and is empty
+if [[ -d $DEVICE ]]; then
+  cd $DEVICE
+  rm ./*
+else
+  echo "New device: ${DEVICE} - creating folder"
+  mkdir $DEVICE
+  cd $DEVICE
+fi
 
+# Copying generated Changelog and .json file and committig changes
+cp "${OUT}/${DEVICE}.json" ./
+cp "${OUT}/Changelog.txt" ./
+cd ..
 git add .
-git commit -m $device': '$(date -d "$D" '+%Y')'-'$(date -d "$D" '+%m')'-'$(date -d "$D" '+%d')' update'
-git push
+git commit -m "${DEVICE}: ${DATE} update"
+
+# Pushing changes
+echo
+git diff HEAD^
+echo
+echo "Make sure changes are correct!"
+echo "Only push after build and md5 checksum are properly uploaded"
+echo -n "Push changes (already committed)? y/[n] > "
+read ans
+if [[ $ans == 'y' ]]; then
+  git push origin HEAD:$BRANCH
+fi
